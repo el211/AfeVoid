40ee30eee949669d47f9fc64ce1b1c956f937c92
AfeliaMc <86126606+el211@users.noreply.github.com>
2024-02-06 13:53:35 +0100

    Update AfeVoid.java
    - An @EventHandler method named onServerLoad was added to handle the ServerLoadEvent. This event is triggered when the server is loaded or restarted.
    The resetVoidWorld method is called during server load to unload and reload the "afevoid" world.
    
    - Added ServerCommandEvent Handler:
    Another @EventHandler method named onServerShutdown was added to handle the ServerCommandEvent. This event is triggered when a server command is executed.
    
    - Updated loadVoidWorld Method:
    The loadVoidWorld method was modified to use the world name "afevoid" when checking for the existence of the void world.
    This ensures that the plugin looks for the "afevoid" world specifically.
    
    - Updated createVoidWorld Method:
    The createVoidWorld method was updated to use the world name "afevoid" when creating the void world. 
    
    - Updated saveVoidWorld Method:
    The saveVoidWorld method was added to save the "afevoid" world before server shutdown.
    This method is called when the server is about to shut down.
    
    - Updated resetVoidWorld Method:
    The resetVoidWorld method was added to unload and reload the "afevoid" world during server load.
    This method is called during server load to ensure the void world is correctly reset.

--- src/main/java/com/example/afeliavoid/AfeVoid.java
+++ src/main/java/com/example/afeliavoid/AfeVoid.java
@@ -1,67 +1,25 @@
-package com.example.AfeliaVoid;
+package com.example.voidafeliagen;
 
+import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
 
-public class AfeVoid extends JavaPlugin implements Listener {
+public class VoidGenerator extends ChunkGenerator {
 
     @Override
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("AfeVoid has been enabled!");
-    }
+    public ChunkGenerator.ChunkData generateChunkData(World world, Random random, int x, int z, ChunkGenerator.BiomeGrid biome) {
+        ChunkGenerator.ChunkData chunkData = createChunkData(world);
 
-    @Override
-    public void onDisable() {
-        getLogger().info("AfeVoid has been disabled!");
-    }
-
-    @EventHandler
-    public void onWorldInit(WorldInitEvent event) {
-        World world = event.getWorld();
-        getLogger().info("WorldInitEvent for world: " + world.getName());
-
-        // Check if the world is a void world
-        if (world.getGenerator() == null || world.getGenerator() instanceof VoidGenerator) {
-            getLogger().info("Void world detected: " + world.getName());
-        }
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (label.equalsIgnoreCase("afevoid")) {
-            createVoidWorld();
-            sender.sendMessage("Void world created!");
-            return true;
-        }
-        return false;
-    }
-
-    private void createVoidWorld() {
-        String worldName = "void";
-        if (getServer().getWorld(worldName) != null) {
-            getLogger().info("Void world already exists!");
-            return;
+        for (int y = 0; y < world.getMaxHeight(); y++) {
+            for (int relativeX = 0; relativeX < 16; relativeX++) {
+                for (int relativeZ = 0; relativeZ < 16; relativeZ++) {
+                    chunkData.setBlock(relativeX, y, relativeZ, Material.AIR);
+                }
+            }
         }
 
-        WorldCreator worldCreator = new WorldCreator(worldName);
-        worldCreator.generator(new VoidGenerator());
-        getServer().createWorld(worldCreator);
-    }
-
-    public static class VoidGenerator extends ChunkGenerator {
-        @Override
-        public ChunkGenerator.ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
-            return createChunkData(world);
-        }
+        return chunkData;
     }
 }
