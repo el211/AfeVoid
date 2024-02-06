fd0f3fd628d2238e9e83ca15d2c02e4792c43205
AfeliaMc <86126606+el211@users.noreply.github.com>
2024-02-06 13:53:58 +0100

    Update AfeVoid.java

--- src/main/java/com/example/afeliavoid/AfeVoid.java
+++ src/main/java/com/example/afeliavoid/AfeVoid.java
@@ -1,25 +1,96 @@
 package com.example.voidafeliagen;
 
-import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerLoadEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
 
-public class VoidGenerator extends ChunkGenerator {
+public class AfeVoid extends JavaPlugin implements Listener {
 
     @Override
-    public ChunkGenerator.ChunkData generateChunkData(World world, Random random, int x, int z, ChunkGenerator.BiomeGrid biome) {
-        ChunkGenerator.ChunkData chunkData = createChunkData(world);
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("AfeVoid has been enabled!");
+        loadVoidWorld();
+    }
 
-        for (int y = 0; y < world.getMaxHeight(); y++) {
-            for (int relativeX = 0; relativeX < 16; relativeX++) {
-                for (int relativeZ = 0; relativeZ < 16; relativeZ++) {
-                    chunkData.setBlock(relativeX, y, relativeZ, Material.AIR);
-                }
-            }
+    @Override
+    public void onDisable() {
+        getLogger().info("AfeVoid has been disabled!");
+    }
+
+    @EventHandler
+    public void onServerLoad(ServerLoadEvent event) {
+        resetVoidWorld();
+    }
+
+    @EventHandler
+    public void onServerShutdown(ServerCommandEvent event) {
+        if (event.getCommand().equalsIgnoreCase("stop")) {
+            saveVoidWorld();
+        }
+    }
+
+    private void loadVoidWorld() {
+        World voidWorld = getServer().getWorld("afevoid");
+        if (voidWorld == null) {
+            getLogger().info("Void world not found. Creating...");
+            createVoidWorld();
+        } else {
+            getLogger().info("Loading void world: " + voidWorld.getName());
+        }
+    }
+
+    private void createVoidWorld() {
+        String worldName = "afevoid";
+        if (getServer().getWorld(worldName) != null) {
+            getLogger().info("Void world already exists!");
+            return;
         }
 
-        return chunkData;
+        WorldCreator worldCreator = new WorldCreator(worldName);
+        worldCreator.generator(new VoidGenerator());
+        getServer().createWorld(worldCreator);
+    }
+
+    private void saveVoidWorld() {
+        World voidWorld = getServer().getWorld("afevoid");
+        if (voidWorld != null) {
+            voidWorld.save();
+            getLogger().info("Void world saved before server shutdown.");
+        }
+    }
+
+    private void resetVoidWorld() {
+        World voidWorld = getServer().getWorld("afevoid");
+        if (voidWorld != null) {
+            getLogger().info("Resetting void world: " + voidWorld.getName());
+
+            // Unload the world
+            getServer().unloadWorld(voidWorld, true);
+
+            // Create and load the void world again
+            createVoidWorld();
+        } else {
+            getLogger().warning("Void world not found during reset.");
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("afevoid")) {
+            createVoidWorld();
+            sender.sendMessage("Void world created!");
+            return true;
+        }
+        return false;
     }
 }
